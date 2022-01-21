66c8faf1199f901b3be7d60efdf2f958b8c3341e
luke boat <lucian.boatright@gmail.com>
2022-01-21 15:57:29 +0000

    tests changed to use Id over Text, also testing rear shifter

--- src/__tests__/cogs.test.js
+++ src/__tests__/cogs.test.js
@@ -1,5 +1,5 @@
 import CogsComp from '@src/components/cogs';
-import { render, screen, cleanup, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 // import Adapter from 'enzyme-adapter-react-16';
 // import { shallow, configure } from 'enzyme';
 
@@ -57,38 +57,52 @@ describe('Testing front shifter for large chainring', () => {
     // expect(onClick).toHaveBeenCalled();
     // ##########
     render(<CogsComp />);
-    fireEvent.click(screen.getByText(/Shift up/i));
+    fireEvent.click(screen.getByTestId('frontShifterUp'));
     const cogLarge = screen.getByTestId('large-cog-discription');
     expect(cogLarge).toHaveTextContent('51');
   });
   it('testing + shifter UP three times returns 53', () => {
     render(<CogsComp />);
-    fireEvent.click(screen.getByText(/Shift up/i));
-    fireEvent.click(screen.getByText(/Shift up/i));
-    fireEvent.click(screen.getByText(/Shift up/i));
+    fireEvent.click(screen.getByTestId('frontShifterUp'));
+    fireEvent.click(screen.getByTestId('frontShifterUp'));
+    fireEvent.click(screen.getByTestId('frontShifterUp'));
     const cogLarge = screen.getByTestId('large-cog-discription');
     expect(cogLarge).toHaveTextContent('53');
   });
   it('testing + shifter changes gear changed gear up once', () => {
     render(<CogsComp />);
-    fireEvent.click(screen.getByText(/Shift down/i));
+    fireEvent.click(screen.getByTestId('frontShifterDown'));
     const cogLarge = screen.getByTestId('large-cog-discription');
     expect(cogLarge).toHaveTextContent('49');
   });
   it('testing + shifter DOWN three times returns 47', () => {
     render(<CogsComp />);
-    fireEvent.click(screen.getByText(/Shift down/i));
-    fireEvent.click(screen.getByText(/Shift down/i));
-    fireEvent.click(screen.getByText(/Shift down/i));
+    fireEvent.click(screen.getByTestId('frontShifterDown'));
+    fireEvent.click(screen.getByTestId('frontShifterDown'));
+    fireEvent.click(screen.getByTestId('frontShifterDown'));
     const cogLarge = screen.getByTestId('large-cog-discription');
     expect(cogLarge).toHaveTextContent('47');
   });
 });
 
-describe('testing cogoutput', () => {
+describe('testing RearShifter', () => {
   it("testing small cog > 15 = top half'", () => {
     render(<CogsComp />);
     const cogLarge = screen.getByTestId('small-cog-discription');
     expect(cogLarge).toHaveTextContent('Top Half');
+  });
+  it('testing small starts at 15', () => {
+    render(<CogsComp />);
+    const cogLarge = screen.getByTestId('small-cog-discription');
+    expect(cogLarge).toHaveTextContent('16');
+  });
+  it('testing RearShifterUp changes small cog up by 1', () => {
+    render(<CogsComp />);
+    // const { getByTestId } = render(
+    //   <button onClick={rearShiftUp}>Shift Up</button>
+    // )
+    fireEvent.click(screen.getByTestId('rearShifterUp'));
+    const cogLarge = screen.getByTestId('small-cog-discription');
+    expect(cogLarge).toHaveTextContent('17');
   });
 });
--- src/components/cogs.tsx
+++ src/components/cogs.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 export default function Cogs() {
   const [largeCog, setLargeCog] = React.useState<number>(50);
-  const [smallCog, setSmallCog] = React.useState<number>(17);
+  const [smallCog, setSmallCog] = React.useState<number>(16);
 
   const frontShiftUp = (event) => {
     console.log('Clicked Front UP');
@@ -16,16 +16,29 @@ export default function Cogs() {
     setLargeCog(largeCog - 1);
   }
 
+  // const rearShiftDown = (event) => {
+  //   console.log('Clicked Front DOWN');
+  //   console.log(smallCog);
+  //   setLargeCog(smallCog - 1);
+  // }
+
+  const rearShiftUp = (event) => {
+    console.log('Clicked Front DOWN');
+    console.log(smallCog);
+    setSmallCog(smallCog + 1);
+  }
+
   return (
     <div className="Cog-Component">
       <div data-testid="cog-component">
         <div data-testid="small-cog-discription">
           {smallCog < 15 ? <div>{smallCog} Bottom half</div> : <div>{smallCog} Top Half</div>}
+          <button data-testid="rearShifterUp" onClick={rearShiftUp}>Shift Up</button>
         </div>
         <div data-testid="large-cog-discription">
           {largeCog < 50 ? <div>{largeCog} Going Easy</div> : <div>{largeCog} Pushing Hard</div>}
-          <button onClick={frontShiftUp}>Shift Up</button>
-          <button onClick={frontShiftDown}>Shift Down</button>
+          <button data-testid="frontShifterUp" onClick={frontShiftUp}>Shift Up</button>
+          <button data-testid="frontShifterDown" onClick={frontShiftDown}>Shift Down</button>
         </div>
         <div>Large cog</div>
       </div>
