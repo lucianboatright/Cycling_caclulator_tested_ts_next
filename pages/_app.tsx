025169ce69d43baba341342a168f48ded8e18e4f
luke boat <lucian.boatright@gmail.com>
2022-01-27 17:41:34 +0000

    pre state adding

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import React, { FC } from 'react';
 import { AppProps } from 'next/app';
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => (
-  // eslint-disable-next-line react/jsx-props-no-spreading
+  // eslint-disable-next-line react/jsx-props-no-spreading.
   <Component {...pageProps} />
 );
 export default MyApp;
