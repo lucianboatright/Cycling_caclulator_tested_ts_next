c7f033c9f74aca20b2e8f65f89b569a14c9f11c5
luke boat <lucian.boatright@gmail.com>
2022-02-02 11:58:12 +0000

    save state test passing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,11 @@
 import React, { FC } from 'react';
 import { AppProps } from 'next/app';
-import { CogGlobalContext } from '@src/context/CogsContext';
+import { CounterProvider } from '@src/context/CogsContext';
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => (
   // eslint-disable-next-line react/jsx-props-no-spreading.
-  <CogGlobalContext>
+  <CounterProvider>
     <Component {...pageProps} />
-  </CogGlobalContext>
+  </CounterProvider>
 );
 export default MyApp;
